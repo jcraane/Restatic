5fac4d7de2f0b3b15f92c305e3933990fbd8abec
Jamie Craane <jcraane@Jamie-Craanes-MacBook-Pro.local>
2011-07-13 23:48:55 +0200

    Added filters for resource bundles to the Configuration object.

--- restatic-core/src/main/java/org/capatect/restatic/core/configuration/Configuration.java
+++ restatic-core/src/main/java/org/capatect/restatic/core/configuration/Configuration.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.Validate;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,14 +15,18 @@ import java.util.List;
  * to do its work. The configuration constists of:
  * <p/>
  * - sourceRootPaths: a list of source folder to scan for resource bundles.
+ * - filters: the filters to use to search for resource bundles. Example: *.properties or just a list of resource bundles.
+ *      Defaults to: *.properties
  *
  * This class is immutable.
  */
 public final class Configuration {
     private List<File> sourceRootPaths;
+    private List<String> filters;
 
     private Configuration(final ConfigurationBuilder builder) {
         this.sourceRootPaths = new ArrayList<File>(builder.sourceRootPaths);
+        this.filters = new ArrayList<String>(builder.filters);
     }
 
     /**
@@ -34,11 +39,24 @@ public final class Configuration {
     }
 
     /**
+     * @return The filters to use to search for resource bundles.
+     */
+    public List<String> getFilters() {
+        return Collections.unmodifiableList(filters);
+    }
+
+    /**
      * @author: Jamie Craane
      */
     public static final class ConfigurationBuilder {
-        private List<File> sourceRootPaths;
+        private static final String DEFAULT_FILTER = "*.properties";
 
+        private List<File> sourceRootPaths;
+        private List<String> filters = Arrays.asList(DEFAULT_FILTER);
+
+        /**
+         * @param sourceRootPaths The source paths to search for resource bundles.
+         */
         public ConfigurationBuilder(final List<File> sourceRootPaths) {
             Validate.notNull(sourceRootPaths, "sourceRootPaths may not be null.");
             Validate.noNullElements(sourceRootPaths, "No sourceRootPath in sourceRootPaths may be null.");
@@ -46,12 +64,16 @@ public final class Configuration {
             this.sourceRootPaths = sourceRootPaths;
         }
 
-        /**
-         * Builds an instance of Configuration from the state of the builder.
-         * @return A new Configuration instance.
-         */
         public Configuration build() {
             return new Configuration(this);
+        }
+
+        public ConfigurationBuilder addFilters(final List<String> filters) {
+            Validate.notNull(filters, "filters may not be null.");
+            Validate.noNullElements(filters, "No filters in filters may be null.");
+
+            this.filters = filters;
+            return this;
         }
     }
 }
--- restatic-core/src/test/java/org/capatect/restatic/core/configuration/ConfigurationBuilderTest.java
+++ restatic-core/src/test/java/org/capatect/restatic/core/configuration/ConfigurationBuilderTest.java
@@ -1,9 +1,11 @@
 package org.capatect.restatic.core.configuration;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
@@ -13,10 +15,16 @@ import static junit.framework.Assert.assertNotNull;
  * @author: Jamie Craane
  */
 public class ConfigurationBuilderTest {
+    private List<File> sourceRootPaths;
+
+    @Before
+    public void before() {
+        sourceRootPaths = new ArrayList<File>();
+        sourceRootPaths.add(new File("/org/capatect/restatic/core/bundleone"));
+    }
+
     @Test
     public void buildWithSourceRootPaths() {
-        List<File> sourceRootPaths = new ArrayList<File>();
-        sourceRootPaths.add(new File("/org/capatect/restatic/core/bundleone"));
         sourceRootPaths.add(new File("/org/capatect/restatic/core/bundletwo"));
         Configuration configuration = new Configuration.ConfigurationBuilder(sourceRootPaths).build();
         assertNotNull(configuration);
@@ -31,7 +39,6 @@ public class ConfigurationBuilderTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void buildWithNullSourceRootPathsInList() {
-        List<File> sourceRootPaths = new ArrayList<File>();
         sourceRootPaths.add(null);
         new Configuration.ConfigurationBuilder(sourceRootPaths).build();
     }
@@ -47,8 +54,50 @@ public class ConfigurationBuilderTest {
 
     @Test(expected = UnsupportedOperationException.class)
     public void testUnmodifiableSourceRootPaths() {
-        List<File> sourceRootPaths = new ArrayList<File>();
-        sourceRootPaths.add(new File("/org/capatect/restatic/core/bundleone"));
         new Configuration.ConfigurationBuilder(sourceRootPaths).build().getSourceRootPaths().remove(0);
+    }
+
+    @Test
+    public void buildWithResourceBundleFilter() {
+        List<String> filters = Arrays.asList("*.properties", "*.xml");
+        Configuration configuration = new Configuration.ConfigurationBuilder(sourceRootPaths).addFilters(filters).build();
+        assertNotNull(configuration.getFilters());
+        assertEquals(2, configuration.getFilters().size());
+        assertEquals("*.properties", filters.get(0));
+        assertEquals("*.xml", filters.get(1));
+    }
+
+    public void defaultFilters() {
+        assertEquals("*.properties", new Configuration.ConfigurationBuilder(sourceRootPaths).build().getFilters().get(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void buildWithNullFilters() {
+        new Configuration.ConfigurationBuilder(sourceRootPaths).addFilters(null).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void buildWithNullFilterInList() {
+        List<String> filters = Arrays.asList("*.resources", null);
+        new Configuration.ConfigurationBuilder(sourceRootPaths).addFilters(filters).build();
+    }
+
+    @Test
+    public void testSafeCopyFilters() {
+        // We cannot use Arrays.asList here as it returns an unmodifiable list. We want to test that the passed-in filters
+        // can NOT be modified.
+        List<String> filters = new ArrayList<String>(){{
+            add("*.properties");
+            add("*.xml");
+        }};
+        Configuration configuration = new Configuration.ConfigurationBuilder(sourceRootPaths).addFilters(filters).build();
+        filters.remove(0);
+        assertEquals(2, configuration.getFilters().size());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testUnmodifiableFilters() {
+        List<String> filters = Arrays.asList("*.resources", "*.xml");
+        new Configuration.ConfigurationBuilder(sourceRootPaths).addFilters(filters).build().getFilters().remove(0);
     }
 }
