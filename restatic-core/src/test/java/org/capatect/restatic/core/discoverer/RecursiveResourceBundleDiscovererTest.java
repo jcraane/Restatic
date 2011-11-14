0e258af8ebfb9771f82dbc70908cf0b36aa2a6f2
Jamie Craane <jcraane@JamieMB.local>
2011-11-14 21:07:48 +0100

    Added Apacge license.

--- restatic-core/src/test/java/org/capatect/restatic/core/discoverer/RecursiveResourceBundleDiscovererTest.java
+++ restatic-core/src/test/java/org/capatect/restatic/core/discoverer/RecursiveResourceBundleDiscovererTest.java
@@ -1,9 +1,24 @@
 package org.capatect.restatic.core.discoverer;
-
-import java.io.File;
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import org.capatect.restatic.core.FileTestUtils;
 import org.junit.Test;
+
+import java.io.File;
 
 /**
  * Tests the discovery of resource bundles in source trees. Since there is no practical way to emulate file systems in memory
